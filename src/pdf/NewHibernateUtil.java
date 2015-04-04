304fbfbf4b47542023033b284e32067124231a42
EdwinNeubauer <edwinneubauer@gmx.de>
2015-04-04 12:24:56 +0200

    Planungspics added

--- src/pdf/NewHibernateUtil.java
+++ src/pdf/NewHibernateUtil.java
@@ -1,3 +1,5 @@
+package pdf;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +12,6 @@ import org.hibernate.SessionFactory;
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
- *
- * @author marco
  */
 public class NewHibernateUtil {
 
