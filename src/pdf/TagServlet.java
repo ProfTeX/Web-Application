7b5dd92564c4b5e82e9a27c9ab47b3b22c645a40
Moritz Bästlein <moritz@baestlein.org>
2015-04-24 07:46:30 +0200

    implemented GET /tag

---
+++ src/pdf/TagServlet.java
@@ -0,0 +1,37 @@
+package pdf;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.*;
+
+/**
+ * Servlet implementation class TagServlet
+ */
+@WebServlet("/tag")
+public class TagServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TagServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Tag> tags = (new TagAccess()).getAllTags();
+		response.getWriter().write(tags.toString());
+	}
+
+}
